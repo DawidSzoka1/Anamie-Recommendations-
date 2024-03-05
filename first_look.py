00e8f7415ec240f72fda6588d8e28bc65d7ac29d
DawidSzoka1 <oficer130@gmail.com>
2024-03-05 17:26:15 +0100

    anime_df and rating_df

--- first_look.py
+++ first_look.py
@@ -5,8 +5,13 @@ import warnings# warning filter
 import scipy as sp #pivot egineering
 
 
-#ML model
-from sklearn.metrics.pairwise import cosine_similarity
-pd.options.display.max_columns
-warnings.filterwarnings("always")
-warnings.filterwarnings("ignore")
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+rating_path = './rating.csv'
+anime_path = './anime.csv'
+
+rating_df = pd.read_csv(rating_path)
+anime_df = pd.read_csv(anime_path)
+print(anime_df.head())
+
