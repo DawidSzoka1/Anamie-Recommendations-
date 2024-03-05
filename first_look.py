9e77ccd9117669a39424ba1dceff5d9bcabccaaa
DawidSzoka1 <oficer130@gmail.com>
2024-03-05 17:16:28 +0100

    first look at project

---
+++ first_look.py
@@ -0,0 +1,12 @@
+import os #paths to file
+import numpy as np # linear algebra
+import pandas as pd # data processing
+import warnings# warning filter
+import scipy as sp #pivot egineering
+
+
+#ML model
+from sklearn.metrics.pairwise import cosine_similarity
+pd.options.display.max_columns
+warnings.filterwarnings("always")
+warnings.filterwarnings("ignore")
